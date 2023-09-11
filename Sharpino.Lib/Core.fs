5cd3339f83ae6e47af5beb9c13a6698ed426959f
Tonyx <tonyx1@gmail.com>
2023-09-11 10:19:02 +0200

    commented the evolve

--- Sharpino.Lib/Core.fs
+++ Sharpino.Lib/Core.fs
@@ -23,17 +23,24 @@ module Core =
                     | Ok h -> h |> e.Process
             ) (h |> Ok)
 
+    // may be this can be refactored:
     let inline evolve<'A, 'E when 'E :> Event<'A>> (h: 'A) (events: List<'E>): Result<'A, string> =
         let rec evolveSkippingErrors (acc: Result<'A, string>) (events: List<'E>) (guard: 'A) =
             match acc, events with
+            // if the accumulator is an error then skip it, and use the guard instead which was the 
+            // latest valid value of the accumulator
             | Error err, _::es -> 
                 // you may want to print or log this
                 printf "warning 1: %A\n" err
                 evolveSkippingErrors (guard |> Ok) es guard
+            // if the accumulator is error and the list is empty then we are at the end, and so we just
+            // get the guard as the latest valid value of the accumulator
             | Error err, [] -> 
                 // you may want to print or log this
                 printf "warning 2: %A\n" err
                 guard |> Ok
+            // if the accumulator is Ok and the list is not empty then we use a new guard as the value of the 
+            // accumulator processed if is not error itself, otherwise we keep using the old guard
             | Ok state, e::es ->
                 let newGuard = state |> e.Process
                 match newGuard with
