fa7e121e1cdb4577673fe49395bef701504b6665
Tonyx <tonyx1@gmail.com>
2024-01-01 13:58:58 +0100

    added TailCall attribute in evolve

--- Sharpino.Lib.Core/Core.fs
+++ Sharpino.Lib.Core/Core.fs
@@ -29,30 +29,30 @@ module Core =
                     | Ok h -> h |> e.Process
             ) (h |> Ok)
 
-    // warning: I cannot write this function less convoluted than this, at the moment
-    let inline evolve<'A, 'E when 'E :> Event<'A>> (h: 'A) (events: List<'E>): Result<'A, string> =
-        let rec evolveSkippingErrors (acc: Result<'A, string>) (events: List<'E>) (guard: 'A) =
-            match acc, events with
-            // if the accumulator is an error then skip it, and use the guard instead which was the 
-            // latest valid value of the accumulator
-            | Error err, _::es -> 
-                log.Info (sprintf "warning 1: %A" err)
+    [<TailCall>]
+    let rec private evolveSkippingErrors (acc: Result<'A, string>) (events: List<Event<'A>>) (guard: 'A) =
+        match acc, events with
+        // if the accumulator is an error then skip it, and use the guard instead which was the 
+        // latest valid value of the accumulator
+        | Error err, _::es -> 
+            log.Info (sprintf "warning 1: %A" err)
+            evolveSkippingErrors (guard |> Ok) es guard
+        // if the accumulator is error and the list is empty then we are at the end, and so we just
+        // get the guard as the latest valid value of the accumulator
+        | Error err, [] -> 
+            log.Info (sprintf "warning 2: %A" err)
+            guard |> Ok
+        // if the accumulator is Ok and the list is not empty then we use a new guard as the value of the 
+        // accumulator processed if is not error itself, otherwise we keep using the old guard
+        | Ok state, e::es ->
+            let newGuard = state |> e.Process
+            match newGuard with
+            | Error err -> 
+                log.Info (sprintf "warning 3: %A" err)
                 evolveSkippingErrors (guard |> Ok) es guard
-            // if the accumulator is error and the list is empty then we are at the end, and so we just
-            // get the guard as the latest valid value of the accumulator
-            | Error err, [] -> 
-                log.Info (sprintf "warning 2: %A" err)
-                guard |> Ok
-            // if the accumulator is Ok and the list is not empty then we use a new guard as the value of the 
-            // accumulator processed if is not error itself, otherwise we keep using the old guard
-            | Ok state, e::es ->
-                let newGuard = state |> e.Process
-                match newGuard with
-                | Error err -> 
-                    log.Info (sprintf "warning 3: %A" err)
-                    evolveSkippingErrors (guard |> Ok) es guard
-                | Ok h' ->
-                    evolveSkippingErrors (h' |> Ok) es h'
-            | Ok h, [] -> h |> Ok
+            | Ok h' ->
+                evolveSkippingErrors (h' |> Ok) es h'
+        | Ok h, [] -> h |> Ok
 
+    let evolve<'A, 'E when 'E :> Event<'A>> (h: 'A) (events: List<Event<'A>>): Result<'A, string> =
         evolveSkippingErrors (h |> Ok) events h
