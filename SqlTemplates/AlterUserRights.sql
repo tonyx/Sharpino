abaf8961cfee69b65cccc4659231f0b23553b00d
Tonyx <tonyx1@gmail.com>
2024-02-26 14:16:45 +0100

    sql templates

---
+++ SqlTemplates/AlterUserRights.sql
@@ -0,0 +1,16 @@
+
+-- migrate:up 
+
+GRANT ALL ON TABLE public.events{ContextVersion}{ContextName} TO safe;
+GRANT ALL ON TABLE public.snapshots{ContextVersion}{ContextName} TO safe;
+GRANT ALL ON SEQUENCE public.snapshots{ContextVersion}{ContextName}_id_seq TO safe;
+GRANT ALL ON SEQUENCE public.events{ContextVersion}{ContextName}_id_seq TO safe;
+          
+GRANT ALL ON TABLE public.aggregate_events{AggregateVersion}{AggregateStorageName} TO safe;
+GRANT ALL ON SEQUENCE public.aggregate_events{AggregateVersion}{AggregateStorageName}_id_seq to safe;
+GRANT ALL ON TABLE public.events{AggregateVersion}{AggregateStorageName} to safe;
+GRANT ALL ON TABLE public.snapshots{AggregateVersion}{AggregateStorageName} to safe;
+GRANT ALL ON SEQUENCE public.snapshots{AggregateVersion}{AggregateStorageName}_id_seq to safe;
+GRANT postgres to safe;    -- dangerous zone!!!! This is to allow at applicative level the classic optimistic lock aggregateState check.
+    
+-- migrate:down
---
+++ SqlTemplates/ContextTemplate.sql
@@ -0,0 +1,64 @@
+-- migrate:up
+
+CREATE TABLE public.events{Version}{ContextStorageName} (
+                                          id integer NOT NULL,
+                                          event json NOT NULL,
+                                          published boolean NOT NULL DEFAULT false,
+                                          kafkaoffset BIGINT,
+                                          kafkapartition INTEGER,
+                                          "timestamp" timestamp without time zone NOT NULL
+);
+
+ALTER TABLE public.events{Version}{ContextStorageName} ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.events{Version}{ContextStorageName}_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+CREATE SEQUENCE public.snapshots{Version}{ContextStorageName}_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE TABLE public.snapshots{Version}{ContextStorageName} (
+                                             id integer DEFAULT nextval('public.snapshots{Version}{ContextStorageName}_id_seq'::regclass) NOT NULL,
+                                             snapshot json NOT NULL,
+                                             event_id integer NOT NULL,
+                                             "timestamp" timestamp without time zone NOT NULL
+);
+
+ALTER TABLE ONLY public.events{Version}{ContextStorageName}
+    ADD CONSTRAINT events{ContextStorageName}_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.snapshots{Version}{ContextStorageName}
+    ADD CONSTRAINT snapshots{ContextStorageName}_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.snapshots{Version}{ContextStorageName}
+    ADD CONSTRAINT event{Version}{ContextStorageName}_fk FOREIGN KEY (event_id) REFERENCES public.events{Version}{ContextStorageName}(id) MATCH FULL ON DELETE CASCADE;
+
+
+CREATE OR REPLACE FUNCTION insert{Version}{ContextStorageName}_event_and_return_id(
+    IN event_in TEXT
+)
+RETURNS int
+       
+LANGUAGE plpgsql
+AS $$
+DECLARE
+inserted_id integer;
+BEGIN
+INSERT INTO events{Version}{ContextStorageName}(event, timestamp)
+VALUES(event_in::JSON, now()) RETURNING id INTO inserted_id;
+return inserted_id;
+
+END;
+$$
+
+
+-- migrate:down
+
