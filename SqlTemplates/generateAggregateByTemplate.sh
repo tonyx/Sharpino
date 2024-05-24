4f2d324c0aa1c07821a667a885f41bc642fe5b22
Tonino Lucca <tonyx1@gmail.com>
2024-05-24 17:22:48 +0200

    fix template again

--- SqlTemplates/generateAggregateByTemplate.sh
+++ SqlTemplates/generateAggregateByTemplate.sh
@@ -2,4 +2,4 @@
 ## give the versiona as first parameter and storagename as second parameter and test/json as third parameter
 ## remember that the third parameter is highly dependent on the config as (test/json) (appSettings.json)
 
-cat AggregateTemplate.sql | sed -e "s/{Version}/$1/g" | sed -e "s/{AggregateStorageName}/$2/g" |> "s/{Format}/$3/g"
+cat AggregateTemplate.sql | sed -e "s/{Version}/$1/g" | sed -e "s/{AggregateStorageName}/$2/g" | sed -e "s/{Format}/$3/g"
