3be484db809a7f338d793faa76d75d57fc845418
Tonino Lucca <tonyx1@gmail.com>
2025-12-12 10:42:43 +0100

    ljklljl

--- Sharpino.Sample.14/TestUtils.fs
+++ Sharpino.Sample.14/TestUtils.fs
@@ -1,2 +1,15 @@
-module Sharpino.Sample.14.TestUtils
+module testUtis
 
+open System
+open Expecto
+open Sharpino.Sample._11.Definitions
+
+[<Tests>]
+let testUtils =
+    testList "testUtils" [
+        testCase "the id is always unique" <| fun _ ->
+            let courseId1 = CourseId.New
+            let courseId2 = CourseId.New
+            Expect.notEqual courseId1.Id courseId2.Id "should be different"
+            Expect.notEqual courseId1 courseId2 "should be different"
+    ]
