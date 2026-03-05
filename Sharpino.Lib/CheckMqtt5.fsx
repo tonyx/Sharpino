6b24550316571593675b89953463907141604b62
Tonino Lucca <tonyx1@gmail.com>
2026-03-05 17:17:28 +0100

    delete temp check files

--- Sharpino.Lib/CheckMqtt5.fsx
+++
@@ -1,8 +0,0 @@
-#r "nuget: MQTTnet, 5.1.0.1559"
-open MQTTnet
-
-let msg = new MqttApplicationMessage()
-// check if ConvertPayloadToString exists
-let methods = msg.GetType().GetMethods() |> Array.filter (fun m -> m.Name.Contains("Convert"))
-for m in methods do
-    printfn "Method: %s" m.Name
